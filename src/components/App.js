50d723eb066ee49ce3081ce088fc6eaeb17a3218
Quyen Tran <qtrandev@gmail.com>
2018-07-01 18:37:52 +0700

    Add styles to list

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Footer from './Footer'
 import AddItem from '../containers/AddItem'
 import VisibleItemList from '../containers/VisibleItemList'
+import '../App.css';
 
 const App = () => (
   <div>
