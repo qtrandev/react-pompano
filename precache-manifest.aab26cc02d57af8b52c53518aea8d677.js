17e6118d4fb29d17357e11df5552fff3fdbadf06
Quyen Tran <qtrandev@gmail.com>
2019-09-12 20:53:57 +0700

    Updates

---
+++ precache-manifest.aab26cc02d57af8b52c53518aea8d677.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d44ee4c523428447874073543389351",
+    "url": "/react-pompano/index.html"
+  },
+  {
+    "revision": "e0a1f1977015b2000a1b",
+    "url": "/react-pompano/static/css/main.46cc8ed0.chunk.css"
+  },
+  {
+    "revision": "1492ac5e5230a9a8e14b",
+    "url": "/react-pompano/static/js/2.c31dfd65.chunk.js"
+  },
+  {
+    "revision": "e0a1f1977015b2000a1b",
+    "url": "/react-pompano/static/js/main.45a6da94.chunk.js"
+  },
+  {
+    "revision": "f680d4e635d352239a8d",
+    "url": "/react-pompano/static/js/runtime~main.f988da4a.js"
+  },
+  {
+    "revision": "23abced930f1868c6a5b976771f231dd",
+    "url": "/react-pompano/static/media/InspectionDataSample.23abced9.csv"
+  }
+]);
